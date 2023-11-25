97a18c41f3f96a579a304261b7c04c60ae53ee5d
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2023-11-25 15:30:51 +0000

    updated the qa owl app

--- notebooks/1_llms_intro_qa_app.py
+++ notebooks/1_llms_intro_qa_app.py
@@ -8,8 +8,49 @@ from langchain.vectorstores import Chroma
 from datetime import datetime
 import os
 import glob
+import requests
 
 st.session_state["chat_history"] = []
+
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
+
+
+def download_docs():
+    # List of file URLs (raw GitHub content URLs)
+    file_urls = [
+        "https://raw.githubusercontent.com/EnkrateiaLucca/oreilly_live_training_llm_apps/main/notebooks/docs/james.txt",
+        "https://raw.githubusercontent.com/EnkrateiaLucca/oreilly_live_training_llm_apps/main/notebooks/docs/maria.txt",
+        "https://raw.githubusercontent.com/EnkrateiaLucca/oreilly_live_training_llm_apps/main/notebooks/docs/sofia.txt",
+        "https://raw.githubusercontent.com/EnkrateiaLucca/oreilly_live_training_llm_apps/main/notebooks/docs/tom.txt",
+        "https://raw.githubusercontent.com/EnkrateiaLucca/oreilly_live_training_llm_apps/main/notebooks/docs/robert.txt",
+        # Add more file URLs as needed
+    ]
+
+    # Directory where files will be downloaded
+    download_dir = "docs"
+    os.makedirs(download_dir, exist_ok=True)
+
+    txt_docs = []
+
+    for url in file_urls:
+        # Extract filename from URL
+        filename = url.split("/")[-1]
+        local_file_path = os.path.join(download_dir, filename)
+
+        # Download and save the file
+        download_file(url, local_file_path)
+
+        # Load the content of the file
+        with open(local_file_path, 'r') as file:
+            content = file.read()
+            txt_docs.append(content)
+
 
 def qa_over_docs_setup(openai_api_key):
     if openai_api_key != "":
@@ -18,7 +59,7 @@ def qa_over_docs_setup(openai_api_key):
         st.error("Please enter your OpenAI API key")
         return
     
-    loader = DirectoryLoader("https://github.com/EnkrateiaLucca/oreilly_live_training_llm_apps/tree/main/notebooks/docs", glob="*.txt")
+    loader = DirectoryLoader("./docs", glob="*.txt")
     txt_docs = loader.load_and_split()
     embeddings = OpenAIEmbeddings()
     txt_docsearch = Chroma.from_documents(txt_docs, embeddings)
@@ -29,7 +70,7 @@ def qa_over_docs_setup(openai_api_key):
 
 def show_documents():
     st.subheader("Documents")
-    for doc in glob.glob("https://github.com/EnkrateiaLucca/oreilly_live_training_llm_apps/tree/main/notebooks/docs/*.txt"):    
+    for doc in glob.glob("./docs/*.txt"):    
         with open(doc, "r") as f:
             st.write(doc)
             st.write(f.read())
@@ -39,6 +80,9 @@ def main():
     st.image("https://github.com/EnkrateiaLucca/oreilly_live_training_llm_apps/blob/main/notebooks/assets-resources/owl.png?raw=true", width=400)
     openai_api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
     show_docs = st.sidebar.checkbox("Show documents")
+    with st.spinner():
+        download_docs()
+    
     chat_history = []
     qa = qa_over_docs_setup(openai_api_key)
     question = st.text_input("What is your question?")
