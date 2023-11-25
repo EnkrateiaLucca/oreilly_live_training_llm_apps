6137d6d809d65fee4a6c9cbe731479629f97dbbd
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2023-11-25 14:42:52 +0000

    updated requirements

--- notebooks/1_intro_llms_panda_app.py
+++ notebooks/1_intro_llms_panda_app.py
@@ -12,10 +12,18 @@ def get_response(prompt):
     return response.choices[0].message.content
 
 st.title("The Panda Warrior")
+prompt_background_panda = st.sidebar.text_area("Write a different background story for the panda")
 st.image("assets-resources/panda_letter.png", width=400)
 
+
+
 if st.button("Write letter"):
-    prompt = "Write a short love letter in 5 sentences from a 1700th century Panda who's been at war and is now returning to his lovely Panda Lady Pandesque."
+    if prompt_background_panda != "":
+        prompt = f"{prompt_background_panda}. Write a short love letter in 5 sentences."
+    else:
+        prompt_background_panda = "You are a 1700th century Panda who's been at war and is now returning to his lovely Panda Lady Pandesque."
+        prompt = f"{prompt_background_panda}. Write a short love letter in 5 sentences."
+    
     response = get_response(prompt)
     st.write(response)
     
