84c5047f90cdc21065a342f7d9c0fd0a7c45aad5
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2023-11-25 14:50:58 +0000

    upated the app and added openai api key setup

--- notebooks/1_intro_llms_panda_app.py
+++ notebooks/1_intro_llms_panda_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 from openai import OpenAI
 
+openai_api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+
 def get_response(prompt):
     client = OpenAI()
+    if openai_api_key != "":
+        client.api_key = openai_api_key
+    else:
+        st.error("Please enter your OpenAI API key")
+        return
     response = client.chat.completions.create(model="gpt-3.5-turbo-1106", 
                              messages=
                              [
@@ -13,6 +20,8 @@ def get_response(prompt):
 
 st.title("The Panda Warrior")
 prompt_background_panda = st.sidebar.text_area("Write a different background story for the panda")
+
+
 st.image("https://github.com/EnkrateiaLucca/oreilly_live_training_llm_apps/blob/main/notebooks/assets-resources/panda_letter.png", width=400)
 
 
