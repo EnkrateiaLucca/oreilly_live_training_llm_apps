d3c878dc7482b70680f8aa4e9fefc73e3f7c6197
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2023-11-25 15:23:30 +0000

    deploying the owl qa app

--- notebooks/1_llms_intro_qa_app.py
+++ notebooks/1_llms_intro_qa_app.py
@@ -6,30 +6,55 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from datetime import datetime
+import os
+import glob
 
-st.title("Q&A")
-st.image("./assets-resources/owl.png", width=400)
-
-# loader = DirectoryLoader("./docs/", glob="*.txt")
-# txt_docs = loader.load_and_split()
-
-# embeddings = OpenAIEmbeddings()
-# txt_docsearch = Chroma.from_documents(txt_docs, embeddings)
-
-# llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2)
-
-# chat_history = []
-# qa = ConversationalRetrievalChain.from_llm(llm, retriever=txt_docsearch.as_retriever())
 st.session_state["chat_history"] = []
 
-question = st.text_input("What is your question?")
-
-if question!="":
-    try:
-        response = qa({"question": f"{question}", "chat_history": st.session_state["chat_history"]})
-        answer = response["answer"]
-        st.session_state["chat_history"].append((question, answer))
-    except Exception as e:
-        answer = "<b>Error:</b> " + str(e)
+def qa_over_docs_setup(openai_api_key):
+    if openai_api_key != "":
+        os.environ["OPENAI_API_KEY"] = openai_api_key
+    else:
+        st.error("Please enter your OpenAI API key")
+        return
     
-    st.write(answer)
+    loader = DirectoryLoader("./docs/", glob="*.txt")
+    txt_docs = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    txt_docsearch = Chroma.from_documents(txt_docs, embeddings)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.0)
+    qa = ConversationalRetrievalChain.from_llm(llm, retriever=txt_docsearch.as_retriever())
+    
+    return qa
+
+def show_documents():
+    st.subheader("Documents")
+    for doc in glob.glob("./docs/*.txt"):    
+        with open(doc, "r") as f:
+            st.write(doc)
+            st.write(f.read())
+
+def main():
+    st.title("Q&A")
+    st.image("https://github.com/EnkrateiaLucca/oreilly_live_training_llm_apps/blob/main/notebooks/assets-resources/owl.png?raw=true", width=400)
+    openai_api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+    show_docs = st.sidebar.checkbox("Show documents")
+    chat_history = []
+    qa = qa_over_docs_setup(openai_api_key)
+    question = st.text_input("What is your question?")
+
+    if question!="":
+        try:
+            response = qa({"question": f"{question}", "chat_history": st.session_state["chat_history"]})
+            answer = response["answer"]
+            st.session_state["chat_history"].append((question, answer))
+        except Exception as e:
+            answer = "<b>Error:</b> " + str(e)
+        
+        st.write(answer)
+    
+    if show_docs:
+        show_documents()
+
+if __name__ == "__main__":
+    main()
