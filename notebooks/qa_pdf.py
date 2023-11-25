f403ce48f5a8341ea0bdbfd1bfd99abf88489ad0
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2023-11-25 18:50:21 +0000

    qa pdf app

--- notebooks/qa_pdf.py
+++ notebooks/qa_pdf.py
@@ -1,23 +1,30 @@
 import streamlit as st
 from langchain.document_loaders import PyPDFLoader
+import PyPDF2
+from io import BytesIO
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.schema import Document
 from langchain.vectorstores import Chroma
 from langchain.chains import ChatVectorDBChain
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
  
+def setup_pdf(file):
+    """Read a PDF file and return its content as text."""
+    reader = PyPDF2.PdfReader(BytesIO(file.read()))
+    docs = []
+    for page in range(len(reader.pages)):
+        docs.append(Document(page_content=reader.pages[page].extract_text()))
+    return docs
 
 
-def setup_pdf_qa(pdf):
-    st.session_state.pdf = pdf
-    loader = PyPDFLoader(pdf)    
-    pdf_doc = loader.load_and_split()
-    return pdf_doc
-
-
-
-def setup_qa_chain(pdf_doc):
+def setup_qa_chain(pdf_doc, openai_api_key):
+    if openai_api_key != "":
+        os.environ["OPENAI_API_KEY"] = openai_api_key
+    else:
+        st.error("Please enter your OpenAI API key")
+        return
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000, chunk_overlap = 500)
     persist_directory = './persist_directory/'
     embedding = OpenAIEmbeddings()
@@ -28,23 +35,26 @@ def setup_qa_chain(pdf_doc):
     pdf_qa = ChatVectorDBChain.from_llm(ChatOpenAI(temperature=0,\
     model_name="gpt-4"), vectordb)
     return pdf_qa
- 
+
+
 def main():
     st.header("Q&A with PDF 💬")
-    # upload a PDF file
-    pdf = st.text_input("Copy the path to the PDF here:")
- 
-    # st.write(pdf)
-    if st.button("Load PDF"):
-        st.session_state.pdf_doc = setup_pdf_qa(pdf)
-        st.write("PDF loaded!")
+    openai_api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+    if uploaded_file is not None:
+        # To read file as string:
+        pdf_docs = setup_pdf(uploaded_file)
+        pdf_qa = setup_qa_chain(pdf_docs,openai_api_key)
+        st.write("Pdf Loaded!")
+    
     # Accept user questions/query
     query = st.text_input("Ask questions about your PDF file:")
-    # st.write(query)
+    st.write(f"Question: {query}")
     if query:
-        pdf_qa = setup_qa_chain(st.session_state.pdf_doc)
         result = pdf_qa({"question": query, "chat_history": []})
-        st.write(result["answer"])
+        answer = result["answer"]
+        st.write(f"Answer: {answer}")
  
 if __name__ == '__main__':
     main()
